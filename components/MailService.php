ec2bd8c94dcbbfaa6b22734911b6f1a10126d099
Yura Griga <grigach@gmail.com>
2014-08-22 13:06:30 +0300

    send message

--- components/MailService.php
+++ components/MailService.php
@@ -1,19 +1,40 @@
 <?php
+
 /** Created by griga at 28.06.2014 | 15:36.
- * 
+ *
  */
+class MailService extends CComponent
+{
 
-class MailService extends CComponent {
-
-    public static function send($templateName, $templateData, $address){
+    /**
+     * @param string $templateName
+     * @param [] $templateData
+     * @param string $address
+     */
+    public static function send($templateName, $templateData, $address)
+    {
         /** @var Template $template */
-        $template = Template::model()->localized()->find('t.key="'.$templateName.'"');
+        $template = Template::model()->localized()->find('t.key="' . $templateName . '"');
+        self::sendMessage(app()->mustache->compile($template->name, $templateData),
+            app()->mustache->compile($template->content, $templateData),
+            $address);
+
+    }
+
+
+    /**
+     * @param string $subject
+     * @param string $message
+     * @param string $to
+     */
+    public static function sendMessage($subject, $message, $to)
+    {
         /** @var EMailer|PHPMailer $mailer */
         $mailer = new EMailer();
         $mailer->IsSMTP();
         $mailer->IsHTML();
         $mailer->CharSet = 'UTF-8';
-        $mailer->SMTPAuth   = true;
+        $mailer->SMTPAuth = true;
         $mailer->SMTPSecure = "tls";
         $mailer->Host = Config::get('mail.host');
         $mailer->Port = Config::get('mail.port');
@@ -22,9 +43,9 @@ class MailService extends CComponent {
         $mailer->From = Config::get('site_email_address');
         $mailer->FromName = Config::get('site_email_from');
         $mailer->AddReplyTo(Config::get('site_email_address'));
-        $mailer->AddAddress($address);
-        $mailer->Subject = app()->mustache->compile($template->name,$templateData);
-        $mailer->Body = app()->mustache->compile($template->content,$templateData);
+        $mailer->AddAddress($to);
+        $mailer->Subject = $subject;
+        $mailer->Body = $message;
         $mailer->Send();
     }
 
